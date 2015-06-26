e0776898757146dcf68467df26d0de1acc23b9c5
Dmitry Avtonomov <dmitriy.avtonomov@gmail.com>
2015-06-26 17:51:03 -0400

    added autoupdate ui

---
+++ Autoupdater/src/umich/ms/test/autoupdater/FakeClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package umich.ms.test.autoupdater;
+
+/**
+ *
+ * @author Dmitry Avtonomov
+ */
+public class FakeClass {
+
+    public FakeClass() {
+    }
+
+    
+}
